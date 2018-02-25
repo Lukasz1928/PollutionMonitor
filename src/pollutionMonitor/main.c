b7741b2ee49add8ea41d2c5b1265f6c8ef418818
Lukasz1928 <Lukasz19281@gmail.com>
2018-02-25 09:53:27 +0100

    Changed main for testing purposes

--- src/pollutionMonitor/main.c
+++ src/pollutionMonitor/main.c
@@ -1,10 +1,15 @@
 #include <avr/io.h>
 #include <avr/delay.h>
 #include "display.h"
+#include "wireless.h"
+#include "SPI.h"
+#include <stdio.h>
 
 int main(void) {
 	display_init();
-	display_write_multiline("line1 abc", "line2 xyz");
+	SPI_master_init();
+	wireless_init();
+	display_write_multiline(a, "");
 	while(1) {
 		
 	}
